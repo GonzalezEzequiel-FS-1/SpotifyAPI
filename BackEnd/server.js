9d3e5a4077e118fae5f28195858d310590e8b554
Zeke <egonzalez@student.fullsail.edu>
2024-11-10 21:50:27 -0500

    modified:   BackEnd/server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -75,7 +75,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', "dist", 'index.html'));
 });
 
 //Finally have the server listen on the defined port. Nice!
