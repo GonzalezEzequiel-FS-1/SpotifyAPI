5827bfa20bb7098972b040e3d900164afdced93c
Zeke <egonzalez@student.fullsail.edu>
2024-11-14 08:00:17 -0500

    ProfileLoading DONE!!!

--- BackEnd/src/utils/getToken.js
+++ BackEnd/src/utils/getToken.js
@@ -2,11 +2,11 @@ const User = require('../models/User');
 
 const getToken = async (user) => {
     const userName = user.user_name
-    console.log(`Formatted ${JSON.stringify(user)} to ${userName}`)
-    console.log('==> Entered getToken function with user:', userName);
+    //console.log(`Formatted ${JSON.stringify(user)} to ${userName}`)
+    //console.log('==> Entered getToken function with user:', userName);
 
     if (!userName) {
-        console.log('No user provided in session');
+        //console.log('No user provided in session');
         return {
             success: false,
             message: 'No user provided in session'
@@ -14,12 +14,12 @@ const getToken = async (user) => {
     }
 
     try {
-        console.log(`==> Searching for user in database: ${userName}`);
+        //console.log(`==> Searching for user in database: ${userName}`);
         const userData = await User.findOne({ user_name: userName });
-        console.log('Database Query Result:', userData);
+        //console.log('Database Query Result:', userData);
 
         if (!userData) {
-            console.log('User not found in database');
+            //console.log('User not found in database');
             return {
                 success: false,
                 message: 'User not found'
@@ -27,28 +27,28 @@ const getToken = async (user) => {
         }
 
         const accessToken = userData.accessToken;
-        console.log('User Access Token:', accessToken);
+        //console.log('User Access Token:', accessToken);
 
         if (!accessToken) {
-            console.log('No access token found for user');
+            //console.log('No access token found for user');
             return {
                 success: false,
                 message: 'No access token found for the user'
             };
         }
 
-        console.log('Access token successfully retrieved');
+        //console.log('Access token successfully retrieved');
         return {
             success: true,
             accessToken: accessToken
         };
 
     } catch (error) {
-        console.log('==> Entering Error checks')
+        //console.log('==> Entering Error checks')
         if(error.response === 401 ){
-            console.log('EXPIRED TOKEN')
+            //console.log('EXPIRED TOKEN')
         }
-        console.error('Error fetching user token:', error.message);
+        //console.error('Error fetching user token:', error.message);
         return {
             success: false,
             message: 'Internal server error during token retrieval',
--- FrontEnd/src/Pages/Profile.jsx
+++ FrontEnd/src/Pages/Profile.jsx
@@ -1,53 +1,87 @@
 import styled from "styled-components";
 import bg from '../assets/bg.jpg';
-import pic from "../assets/pic.png";
-import SubmitBtn from "../Components/Buttons/SubmitBtn";
-import getUserName from "../../utils/getUserName";
-import { useState } from "react";
+// import getUserName from "../../utils/getUserName";
+import { useEffect, useState } from "react";
 import axios from "axios"
+import CircularIndeterminate from "../Components/Progress/CircularProgress";
 
 export default function Profile() {
     // State to hold the username
-    const [userName, setUserName] = useState(null);
+    const [user_name, setUserName] = useState('');
+    const [country, setCountry] = useState('');
+    const [display_name, setDisplayName] = useState('');
+    const [email, setEmail] = useState('');
+    //const [external_url, setExternalUrl] = useState(null);
+    const [followers, setFollowers] = useState(0);
+    const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    // Function to load the data
-    const getName = async () => {
-        
-        const name = await getUserName(); 
-        if (name) {
-            setUserName(name);
-        } else {
-            console.log("Failed to fetch username.");
+    const loadProfile = async () => {
+        setLoading(true);
+        try {
+            const { data } = await axios.get('http://localhost:3069/api/profile', {
+                withCredentials: true,
+            });
+
+            const profile = data.profile;
+
+            setUserName(profile.id || '');
+            setCountry(profile.country || '');
+            setDisplayName(profile.display_name || '');
+            setEmail(profile.email || '');
+            setFollowers(profile.followers?.total || 0);
+            setImages(profile.images || [])
+
+            console.log("Profile Data:", data);
+        } catch (error) {
+            console.error('Error fetching profile:', error.message);
+        } finally {
+            setLoading(false);
         }
     };
-    const loadProfile = async () =>{
-        try{
-        const userProfile = await axios.get('http://localhost:3069/api/profile', {
-            withCredentials: true,
-        })
-        console.log(userProfile.data)
-        }catch(error){
-            console.error(error.message)
-        }
-    }
 
-  return (
-    <Container>
-        <TopBanner></TopBanner>
-        <AvatarContainer>
-            <Avatar src={pic}/>
-        </AvatarContainer>
-        <BottomPortion><SubmitBtn 
-            onClick={getName}
-            text="GET NAME"
-        /><Text>{userName}</Text>
-        <SubmitBtn 
-            onClick={loadProfile}
-            text="LOAD DATA"
-        />
-        </BottomPortion>
-    </Container>
-  )
+    useEffect(() => {
+        loadProfile()
+    }, [])
+    return loading
+        ? (
+            <CircularIndeterminate />
+        ) : (
+            <Container>
+                <TopBanner></TopBanner>
+                <AvatarContainer>
+                    <Avatar src={images[0].url} />
+                </AvatarContainer>
+                <BottomPortion>
+                <Title>User Profile</Title>
+                    <DataContainer>
+                        
+                            <InfoSlot>
+                                <DataTitle>User Name:</DataTitle>
+                                <DataDesc>{user_name}</DataDesc>
+                            </InfoSlot>
+                            <InfoSlot>
+                                <DataTitle>Display Name:</DataTitle>
+                                <DataDesc>{display_name}</DataDesc>
+                            </InfoSlot>
+                            <InfoSlot>
+                                <DataTitle>Country:</DataTitle>
+                                <DataDesc>{country}</DataDesc>
+                            </InfoSlot>
+                        
+                            <InfoSlot>
+                                <DataTitle>Email:</DataTitle>
+                                <DataDesc>{email}</DataDesc>
+                            </InfoSlot>
+                            <InfoSlot>
+                                <DataTitle>Followers:</DataTitle>
+                                <DataDesc>{followers}</DataDesc>
+                            </InfoSlot>
+                        
+                    </DataContainer>
+                </BottomPortion>
+            </Container>
+        )
 }
 
 const Container = styled.div`
@@ -57,7 +91,7 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    
+    overflow: hidden;
 `
 const TopBanner = styled.div`
     width:100%;
@@ -82,15 +116,70 @@ const AvatarContainer = styled.div`
     top:10%
     
 `
+const BottomPortion = styled.div`
+    position:relative;
+    min-width: 400px;
+    width:100%;
+    height:100%;
+    top:20%;
+    display:flex;
+    align-items:flex-start;
+    justify-content: center;
+    gap:2rem;
+`
 const Avatar = styled.img`
     object-fit: cover;
 `
-const BottomPortion = styled.div`
-    width:100%;
-    height:80%;
-    background-color:"#555555"
+const DataTitle = styled.h1`
+    color:#FFFFFF80;
+    font-size:.75rem;
+    font-family: "Catamaran","Sans Serif";
+    text-transform: uppercase;
+    letter-spacing: .05rem;
+    transition: all .5s ease-in-out;
+    &:hover{
+        color:#800000;
+    }
 `
-const Text = styled.h1`
+const DataDesc = styled.h1`
     color:white;
-    font-size:20rem;
+    font-size:1.5rem;
+    font-family: "Palanquin","Sans Serif";
+    text-transform: uppercase;
+    letter-spacing: .05rem;
+    transition: all .5s ease-in-out;
+    &:hover{
+        color:#800000;
+    }
+`
+const Title = styled.p`
+    position:absolute;
+    text-transform: uppercase;
+    letter-spacing: .05rem;
+    color:white;
+    top:-1.5rem;
+    font-family: "Roboto";
+    font-weight: 800;
+    font-size: 2rem;
+    transition: all .5s ease-in-out;
+    &:hover{
+        color:#800000;
+    }
+`
+const InfoSlot = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:0rem;
+    line-height: 1.15rem;
+`
+const DataContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:1rem;
+    background-color: #55555550;
+    padding:2rem;
+    border: 0.15rem solid #aaaaaa50;
+    border-radius: 20px;
+    height:50%;
+    box-shadow: 5px 5px 5px #222222;
 `
