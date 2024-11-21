48806fadc1696a2de918c7e18a9f863bd9c82ed4
Zeke <egonzalez@student.fullsail.edu>
2024-11-21 08:28:35 -0500

    modified:   BackEnd/src/controllers/SpotifyControllers/search.js

--- BackEnd/src/controllers/SpotifyControllers/search.js
+++ BackEnd/src/controllers/SpotifyControllers/search.js
@@ -11,26 +11,32 @@ const search = async (req, res) =>{
             success:false,
             message:`Query not provided`
         })
+        console.log(' Query didnt make it ')
     }
-
+console.log(' Query check pass checking user ')
     if(!user){
         res.status(400).json({
             success:true,
             message:"User not provided"
         })
+        console.log(' user didnt make it ')
     }
-    
+    console.log(' user check passed ')
     try{
+        console.log(' Checking DB ')
         const response = await User.findOne({"user_name":`${user}`})
         //console.log(`FROM SEARCH ====>>${JSON.stringify(response.data)}`)
+  
         const token = response.accessToken
         if(!token){
             res.status(400).json({
                 success:false,
                 message:'No data retrieved from DB'
             })
+            console.log(' no token from DB ')
         }
         if(token){
+            console.log(' got token searching spotify ')
             const search = await axios.get(`https://api.spotify.com/v1/search?q=${query}`, {
                 headers:{
                     "Authorization" : `Bearer ${accessToken}`
@@ -39,6 +45,7 @@ const search = async (req, res) =>{
             console.log(search)  
         }
     }catch(err){
+        console.log(`Error 500>>>> m${err.message}`)
         res.status(500).json({
             success:false,
             error:err.message
